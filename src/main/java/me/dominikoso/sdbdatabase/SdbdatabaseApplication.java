84a5930b68a861d01c3d0bc3e1dbc4ed732c4e6a
dominikoso <rodzinka.kostkow@gmail.com>
2019-03-30 22:44:29 +0100

    Initial commit

---
+++ src/main/java/me/dominikoso/sdbdatabase/SdbdatabaseApplication.java
@@ -0,0 +1,13 @@
+package me.dominikoso.sdbdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdbdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdbdatabaseApplication.class, args);
+	}
+
+}
